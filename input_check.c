a2d1a5d63d4dae4203be09bf655a258ac60ee6d9
cocomhwa <swk951031@gmail.com>
2023-01-24 00:17:12 +0900

    fix : fix files
    input_check.c :
    free error fixed.

--- input_check.c
+++ input_check.c
@@ -6,13 +6,13 @@
 /*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:25:20 by wooshin           #+#    #+#             */
-/*   Updated: 2023/01/22 22:27:42 by wooshin          ###   ########.fr       */
+/*   Updated: 2023/01/24 00:13:21 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	*free_array(int	*array)
+static int	*free_int_array(int	*array)
 {
 	free(array);
 	array = 0;
@@ -43,19 +43,19 @@ int	*input_check(int argc, char **argv)
 		if (!arrays)
 			return (0);
 		size = get_size_arr(arrays);
+		int_array = make_valid_int(size, arrays);
+		free(arrays);
 	}
 	else if (argc > 2)
 	{
-		arrays = argv + 1;
-		size = argc -1;
+		size = argc - 1;
+		int_array = make_valid_int(size, argv + 1);
 	}
 	else
 		return (0);
-	int_array = make_valid_int(size, arrays);
 	if (!int_array)
 		return (0);
-	free(arrays);
 	if (check_duplication(size, int_array))
-		return (free_array(int_array));
+		return (free_int_array(int_array));
 	return (int_array);
 }
