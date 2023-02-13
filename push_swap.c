d940c1417de36a7466a7161b9a450753203076db
cocomhwa <swk951031@gmail.com>
2023-02-13 14:10:26 +0900

    add, fix : add visualizer, fix push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: wooshin <wooshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 23:31:03 by wooshin           #+#    #+#             */
-/*   Updated: 2023/02/08 18:59:56 by wooshin          ###   ########.fr       */
+/*   Updated: 2023/02/13 13:55:26 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	push_swap(t_stack a, t_stack b)
 {
-	print_stack(a, b);
+//	print_stack(a, b);
 	reverse_sort(&a, &b);
-	print_stack(a, b);
+//	print_stack(a, b);
 	complete_sort(&b, &a);
-	print_stack(a, b);
+//	print_stack(a, b);
 	return ;
 }
