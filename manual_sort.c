6aaa7f1321e24036ad11173840cf158ccfbe936c
cocomhwa <swk951031@gmail.com>
2023-02-23 21:59:13 +0900

    fix : fix manual_sort.c
    add feature that 3nums sort

--- manual_sort.c
+++ manual_sort.c
@@ -6,11 +6,42 @@
 /*   By: wooshin <wooshin@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:37:19 by wooshin           #+#    #+#             */
-/*   Updated: 2023/02/23 17:58:49 by wooshin          ###   ########.fr       */
+/*   Updated: 2023/02/23 21:59:06 by wooshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	sort_three_num(t_stack *a)
+{
+	if (a->top->num == 0)
+	{
+		s(a);
+		r(a);
+	}
+	else if (a->top->num == 1)
+	{
+		if(a->top->down->num == 0)
+			s(a);
+		else
+			rr_(a);
+	}
+	else
+	{
+		if (a->top->down->num == 1)
+			r(a);
+		else
+		{
+			s(a);
+			rr_(a);
+		}
+	}
+}
+
+void	sort_five_num()
+{
+	ft_printf("hi");
+}
 
 void	manual_sort(t_stack *a, t_stack *b)
 {
@@ -20,7 +51,7 @@ void	manual_sort(t_stack *a, t_stack *b)
 	if (size == 2)
 		r(a);
 	else if (size == 3)
-		sort_three_num();
+		sort_three_num(a);
 	else if (size == 5)
 		sort_five_num();
 	else
